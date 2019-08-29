5fc7376eddd5b62b4d4d02c9f825e326f4ac413e
NolkMan <4n931mail@gmail.com>
2019-08-29 13:07:03 -0400

    Made a file for tests of methods

---
+++ tests.c
@@ -0,0 +1,21 @@
+#include "stdio.h"
+#include "stdlib.h"
+
+#include "comm_utils.h"
+
+void comm_utils_tests(){
+	char param[] = "1000";
+	char * message = generate_message_with_param(MESSAGE_SCORE, param);
+	printf("Message: \"%s\"\n", message);
+	char *par = NULL;
+	int code = check_for_message(message, &par);
+	printf("Code: %d\nParam: \"%s\"\n", code, par);
+
+	free(par);
+	free(message);
+}
+
+int main(){
+	comm_utils_tests();
+	return 0;
+}
